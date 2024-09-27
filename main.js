bf63b07cc02665b775c6b4d5b4ceec114acb93f6
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2024-09-27 12:40:12 +0000

    add complete button

--- main.js
+++ main.js
@@ -7,24 +7,35 @@ const inputEl = document.querySelector("input");
 const addBtn = document.querySelector("[data-add-btn]");
 const dataList = document.querySelector("[data-list-tasks]");
 
+// Get tasks from local storage
+const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function renderTask(task) {
+  dataList.appendChild(TaskList(task));
+}
+
 function handleInput() {
   const currValue = inputEl.value.trim();
   //  Checking for empty value
-  if (!currValue) {
+  if (!currValue || tasks.length > 5) {
     return;
   }
 
-  dataList.appendChild(TaskList(currValue));
-  console.log(currValue);
+  renderTask(currValue);
 
   //   Empty the input
   inputEl.value = "";
 }
 
+// === Add Listeners ===
 inputEl.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     handleInput();
   }
 });
-
 addBtn.addEventListener("click", handleInput);
+
+// Render tasks on Initial Load
+document.addEventListener("DOMContentLoaded", () => {
+  tasks.forEach(renderTask);
+});
