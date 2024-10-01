840f78a20f64645b19e59622f6fff26eaf0231cb
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2024-10-01 11:52:45 +0000

    Rebuilding UI

--- components/Toast.js
+++ components/Toast.js
@@ -1,13 +1,1 @@
-const showToast = document.querySelector("[data-show-toast]");
-
-export default function (message) {
-  const div = document.createElement("div");
-  div.classList.add("alert", "alert-info");
-  div.innerHTML = `<span>${message}.</span>`;
-  showToast.appendChild(div);
-
-  // Remove after delay
-  setTimeout(() => {
-    showToast.removeChild(div);
-  }, 1400);
-}
+export default function () {}
