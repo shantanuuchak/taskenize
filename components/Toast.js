dac3b4ed3ebea1b5c112619539026643954d16ae
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2024-10-01 12:05:16 +0000

    add toast notifications

--- components/Toast.js
+++ components/Toast.js
@@ -1,1 +1,16 @@
-export default function () {}
+const body = document.querySelector("body");
+
+export default function (title) {
+  const div = document.createElement("div");
+  div.classList.add("toast", "toast-top", "toast-start");
+  div.innerHTML = `
+    <div class="alert alert-info">
+        <span>${title}</span>
+    </div>`;
+
+  body.appendChild(div);
+
+  setTimeout(() => {
+    body.removeChild(div);
+  }, 1500);
+}
--- main.js
+++ main.js
@@ -1,2 +1,17 @@
-// For Tailwind & Daisey
+// For Tailwind & Components
 import "./style.css";
+import Toast from "./components/Toast";
+
+// === DOM Selection ===
+const inputEl = ge("[data-input-text]");
+const btnAdd = ge("[data-btn-add]");
+const btnClearLocal = ge("[data-btn-clearlocal]");
+
+// Get elements from DOM
+function ge(attribute) {
+  return document.querySelector(attribute);
+}
+
+Toast("Page loaded");
+
+const handleChange = () => {};
